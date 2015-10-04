d07c68a084c4ea6deb6ba796274aaa70e4d7b9bc
SerhanU <sulkumen@usc.edu>
2015-10-04 14:54:02 +0300

    Cycle Retinopathy Blotches
    Retinopathy Blotches Cycle on Cardboard Camera
    RetinaChange.cs script

---
+++ Cardboard_Test_Menu/Cardboard_Test_Menu/Assets/HackScripts/RetinaChange.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RetinaChange : MonoBehaviour {
+
+	public List<GameObject> RetinaImages = new List<GameObject>();
+	int index = 0;
+	int currentInt;
+	GameObject currentImage;
+
+	void Start() {
+		//make sure all Retina Images are off
+		for(int i = 0; i < RetinaImages.Count; i++) {
+			if(RetinaImages[i].activeInHierarchy){
+				RetinaImages[i].SetActive(false);
+			}
+		}
+		currentImage = RetinaImages[index];
+		currentImage.SetActive (true);
+		Debug.Log(RetinaImages.Count);
+	}
+	
+	void Update () {
+		//if trigger button is clicked
+		if(Input.GetMouseButtonDown(0)){
+			ChangeToNextImage();
+		}
+	}
+
+	void ChangeToNextImage(){
+		currentImage.SetActive(false);
+		currentInt++;
+
+		if(currentInt < RetinaImages.Count){
+//			Debug.Log ("Current Retina Image: " + currentInt);
+			currentImage = RetinaImages[currentInt];
+			currentImage.SetActive(true);
+		}else if (currentInt == RetinaImages.Count){
+			currentInt = index;
+			currentImage = RetinaImages[currentInt];
+//			Debug.Log ("Current Retina Image: " + currentInt);
+			currentImage.SetActive(true);
+		}
+	}
+}
